75023ba5c18582a816869b96ae69e41088ec266b
TatsuyaNakahara <tatsuya.nakahara@rakuten.com>
2018-06-20 00:19:07 +0000

    tasklist777

--- database/migrations/2018_06_19_152727_add_user_id_to_tasks.php
+++ database/migrations/2018_06_19_152727_add_user_id_to_tasks.php
@@ -16,6 +16,7 @@ class AddUserIdToTasks extends Migration
         Schema::table('tasks', function (Blueprint $table) {
             $table->integer('user_id')->unsigned()->index();
         });
+        $table->foreign('user_id')->references('id')->on('users');
     }
 
     /**
